70dcfe429b7c33262fa62e6f78817f5066cce60a
Egor Ovechkin <egorka1391@gmail.com>
2017-03-27 19:08:53 +0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import express from 'express'
 import promisify from 'es6-promisify'
 import request_cb from 'request'
-
-const app = express()
 const request = promisify(request_cb.post)
 
-request('https://translate.yandex.net//api/v1.5/tr.json/translate?lang=en-ru&key=trnsl.1.1.20170326T141009Z.afd6d8e4e238cde0.83e36ba00fd16f9ba8df819f0d5a4ae4042b58df&&text=Hello world')
-.then(response => console.log(response.body))
-        // if (!error && response.statusCode == 200) {
-        // }
-.catch(err => console.log(err))
+const app = express()
+
+export function translate(text) {
+  request(`https://translate.yandex.net//api/v1.5/tr.json/translate?lang=en-ru&key=trnsl.1.1.20170326T141009Z.afd6d8e4e238cde0.83e36ba00fd16f9ba8df819f0d5a4ae4042b58df&&text=${text}`)
+  .then(response => {
+    console.log('>>>', response.body)
+    return response.body
+  })
+          // if (!error && response.statusCode == 200) {
+          // }
+  .catch(err => console.log(err))
+}
--- src/server.js
+++ src/server.js
@@ -1,19 +1,20 @@
 import promisify from 'es6-promisify'
 import TelegramBot from 'node-telegram-bot-api'
+import { translate } from './index.js'
 
 const token = '339608802:AAFewFYiBYoWyzmDLSYfToqOcAcda_hoviQ'
 const bot = new TelegramBot(token, { polling: true })
 // const bot = promisify(bot_cb.onText)
-// Matches "/echo [whatever]" 
+// Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, function (msg, match) {
-  // 'msg' is the received Message from Telegram 
-  // 'match' is the result of executing the regexp above on the text content 
-  // of the message 
- 
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
   var chatId = msg.chat.id
-  console.log(msg)
-  var resp = match[1] // the captured "whatever" 
- 
-  // send back the matched "whatever" to the chat 
-  bot.sendMessage(chatId, resp)
+  // console.log(msg)
+  var resp = match[1] // the captured "whatever"
+  console.log('>>', translate(match[1]))
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, match[1])
 })
