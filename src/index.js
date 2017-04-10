e19186e5c648a11e97b012b42bf1d11f937bd713
Egor Ovechkin <egorka1391@gmail.com>
2017-04-10 19:12:55 +0500

    add sayit command

--- src/index.js
+++ src/index.js
@@ -2,25 +2,14 @@ import express from 'express'
 import promisify from 'es6-promisify'
 import request_cb from 'request'
 const request = promisify(request_cb.post)
-
-const app = express()
+const get = promisify(request_cb.get)
 
 export function translate(text, distanation) {
-  // let result = {}
   return request(
     `https://translate.yandex.net//api/v1.5/tr.json/translate?`
     + `lang=${distanation}`
     + `&key=trnsl.1.1.20170326T141009Z.afd6d8e4e238cde0.83e36ba00fd16f9ba8df819f0d5a4ae4042b58df`
     + `&text=${text}`)
-  // .then(response => {
-  //   // console.log('>>>', response.body)
-  //   // result = response.body
-  // })
-
-  // return result
-          // if (!error && response.statusCode == 200) {
-          // }
-  // .catch(err => console.log(err))
 }
 
 export function detected(text, hint) {
@@ -33,6 +22,11 @@ export function detected(text, hint) {
 }
 
 export function getAdvice() {
-  console.log('>>>>>', encodeURIComponent('http://fucking-great-advice.ru/api/random_by_tag/кодеру'))
-  return request('http://fucking-great-advice.ru/api/random_by_tag/%D0%BA%D0%BE%D0%B4%D0%B5%D1%80%D1%83')
+  return request('http://fucking-great-advice.ru/api/random')
+}
+
+export function sayIt(text) {
+  console.log(text)
+  return get(
+    `https://tts.voicetech.yandex.net/generate?text=Hello&format=mp3&lang=en-US&speaker=oksana&emotion=good&key=689b5e1f-d62e-45dd-9f37-b16c3c587813`)
 }
--- src/server.js
+++ src/server.js
@@ -2,22 +2,15 @@ import promisify from 'es6-promisify'
 import TelegramBot from 'node-telegram-bot-api'
 import { translate } from './index.js'
 import { detected } from './index.js'
-import { getAdvice } from './index.js'
+import { getAdvice, sayIt } from './index.js'
+import request from 'request'
 
 const token = '339608802:AAFewFYiBYoWyzmDLSYfToqOcAcda_hoviQ'
 const bot = new TelegramBot(token, { polling: true })
-// const bot = promisify(bot_cb.onText)
-// Matches "/echo [whatever]"
-// \/echo
-bot.onText(/\/t(?:@ya_translater_bot)? (.+)/, function (msg, match) {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
+bot.onText(/\/t(?:@ya_translater_bot)? (.+)/, function (msg, match) {
   const chatId = msg.chat.id
-  // console.log(msg)
-  const resp = match[1] // the captured "whatever"
-  // console.log('>>', translate(match[1]))
+  const resp = match[1]
   detected(encodeURIComponent(match[1]), 'ru,en')
   .then(response => {
     console.log('<><><><><><><>\n', JSON.parse(response.body).lang, '\n<><><>><>><')
@@ -26,14 +19,10 @@ bot.onText(/\/t(?:@ya_translater_bot)? (.+)/, function (msg, match) {
     console.log('dist;    ', dist)
     translate(encodeURIComponent(match[1]), dist)
     .then(response => {
-      console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n', JSON.parse(response.body).text, '\n<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
       bot.sendMessage(chatId, JSON.parse(response.body).text[0])
     })
   })
   .catch(err => console.log(err))
-
-  // send back the matched "whatever" to the chat
-  // bot.sendMessage(chatId, match[1])
 })
 
 bot.onText(/\/fuck(?:@ya_translater_bot)?.*/, function (msg, match) {
@@ -41,7 +30,50 @@ bot.onText(/\/fuck(?:@ya_translater_bot)?.*/, function (msg, match) {
   getAdvice()
   .then(response => {
     console.log(response.body)
-    bot.sendMessage(chatId, decodeURIComponent(JSON.parse(response.body).text).replace('&nbsp;', ' '))
+    bot.sendMessage(chatId, decodeURIComponent(JSON.parse(response.body).text).replace('&nbsp;', ' '), { parse_mode: 'HTML' })
   })
   .catch(err => console.log(err))
 })
+
+bot.onText(/\/sayit(?:@ya_translater_bot)?(.*)/, function (msg, match) {
+  console.log(']]]]]]]]]][[[[[[[]]]]]][[[[')
+  const chatId = msg.chat.id
+  // sayIt(match[1])
+  const stream = request
+  .get(`https://tts.voicetech.yandex.net/generate?text=Hello&format=mp3&lang=en-US&speaker=oksana&emotion=good&key=689b5e1f-d62e-45dd-9f37-b16c3c587813`)
+  .on('response', function(response) {
+    console.log(response.statusCode) // 200
+    console.log(response.headers['content-type']) // 'image/png'
+  })
+  bot.sendAudio(chatId, stream)
+
+  // .pipe(bot.sendAudio(chatId, 'doodle.png'))
+    // .then(response => console.lo)
+    // .then(response => {
+    //   // console.log('>>>>>>>>>>\n' + JSON.stringify(response) + '\n<<<<<<<<<<<<')
+    //   // const stream = response.pipe()
+    //   // console.log(stream)
+    //   bot.sendAudio(chatId, response.pipe())
+    // })
+    // .catch(err => console.log(err))
+  // console.log
+  // bot.getChat(chatId)
+  // getAdvice()
+  // .then(response => {
+  //   console.log(response.body)
+  //   bot.sendMessage(chatId, decodeURIComponent(JSON.parse(response.body).text).replace('&nbsp;', ' '), { parse_mode: 'HTML' })
+  // })
+})
+
+// bot.onText(/.*/, function (msg, match) {
+//   const chatId = msg.chat.id
+//   console.log('>>>>>>>\n', msg, '\n<<<<<<<<<')
+  // bot.getChat(chatId)
+  //   .then(chat => console.log('>>>>>>>>>>\n' + JSON.stringify(chat) + '\n<<<<<<<<<<<<'))
+  //   .catch(err => console.log(err))
+  // getAdvice()
+  // .then(response => {
+  //   console.log(response.body)
+  //   bot.sendMessage(chatId, decodeURIComponent(JSON.parse(response.body).text).replace('&nbsp;', ' '), { parse_mode: 'HTML' })
+  // })
+// })
